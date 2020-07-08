9946ba0fc8361ed458eb27f72f1cd2576402ae03
BansariS <43056564+BansariS@users.noreply.github.com>
2020-07-08 03:43:23 -0700

    Update FindDistance.cs

--- Assets/FindDistance.cs
+++ Assets/FindDistance.cs
@@ -17,18 +17,16 @@ public class FindDistance : MonoBehaviour
         if (other)
         {
             float dist = Vector3.Distance(other.position, transform.position);
-            Debug.Log("other.position is " + other.position);
-            Debug.Log("transform.position is " + transform.position);
-            Debug.Log("Distance to other:"+ dist);
+           
             if (dist <= minimumdistance)
             {
                 rectTransform.localScale = new Vector3(1, 1, 1);
-                Debug.Log("scale is not increased");
+                
             }
             else
             {
                 rectTransform.localScale = new Vector3(3f, 3f, 3f);
-                Debug.Log("scale is increased");
+               
             }
         }
     } 
