84c4e270143e23d0da2b22bb4f221ef63a5e214f
BansariS <43056564+BansariS@users.noreply.github.com>
2020-07-08 01:01:55 -0700

    Update VideoScript.cs

--- Assets/VideoScript.cs
+++ Assets/VideoScript.cs
@@ -11,21 +11,18 @@ public class VideoScript : MonoBehaviour
     {
         videoPlayer = GetComponent<VideoPlayer>();
     }
+    
     // Start is called before the first frame update
     void Start()
     {
-
      Invoke("playVideo", 3.0f);
-
-
     }
+    
     void playVideo()
     {
         videoPlayer.Play();
     }
     
-   
-
     // Update is called once per frame
     void Update()
     {
